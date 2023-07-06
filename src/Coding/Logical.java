753e49759281c7f19102037f9f30a577d4a2cb25
sahit <sahit@DESKTOP-L40JIR3>
2023-07-06 18:04:35 +0530

    First commit

---
+++ src/Coding/Logical.java
@@ -0,0 +1,129 @@
+package Coding;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Logical {
+
+	public static void main(String[] args) {
+		
+//		String s ="sahithya is cute" ;
+//		
+//		int count = 0;
+//		
+//		String sp[] = s.split(" ");
+//		
+//		for(int i=0;i<sp.length;i++) {
+//			count++;
+//		}
+//			
+//		System.out.println(count);	
+//			
+		
+//		int arr [] = {25,89,56,89,98,47,21,23,44,97};
+//		
+//		int size = arr.length;
+//		
+//		int temp ;
+//		
+//		for(int i=0;i<arr.length;i++) {
+//			for(int j=i+1;j<arr.length;j++) {
+//				if(arr[i]>arr[j]) {
+//					temp=arr[i];
+//					arr[i]=arr[j];
+//				    arr[j]=temp;
+//				}
+//			}
+//		}
+		
+		//System.out.println(arr[size-2]);
+		
+	//	System.out.println(Arrays.toString(arr));
+		
+	//System.out.println(arr[1]);	
+		
+		
+//		ArrayList<String> str = new ArrayList<String>();
+//		
+//		str.add("sahithya");
+//		
+//		str.add("sahithi");
+//		
+//		str.add("chikky");
+//		
+//		str.add("sai");
+//		
+//		System.out.println(str);
+//		
+//		System.out.println("...............................................................................");
+//		
+//		Collections.swap(str, 0, 3);
+//		
+//		System.out.println(str);
+		
+		
+		
+//	String s ="sahithya";
+//	
+	String rev = " ";
+//	
+//	for(int i=0;i<s.length();i++) {
+//	rev=s.charAt(i)+rev;
+//	}
+//		
+//	System.out.println(rev);	
+
+//		
+//		String s ="my name is sahithya";
+//		
+//		String sp[] =s.split(" ");
+//		
+//		for(int i=0;i<sp.length;i++) {
+//			rev= sp[i]+" "+rev;
+//		}
+//		
+//		System.out.println(rev);
+		
+		
+//	String s = "sahithya";
+//	
+//	for(int i=0;i<s.length();i++) {
+//		for(int j=i+1;j<s.length()+1;j++) {
+//			System.out.println(s.substring(i, j));
+//		}
+//	}
+	
+	
+//	String hungry = "yes";
+//	
+//	if(hungry=="yes"||hungry=="y"||hungry=="Y") {
+//		System.out.println("Avunu");
+//	}
+//	else {
+//		System.out.println("Ledhu");
+//	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+		
+		
+		
+		
+	}
+
+}
